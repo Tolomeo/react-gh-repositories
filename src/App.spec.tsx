a4d231acab8125398281f16c7ad4c379f3ef5b06
Diego Frattini <dgfrattini@gmail.com>
2022-12-03 23:17:24 +0000

    Repositories tests

--- src/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { render } from '@testing-library/react'
-import App from './App'
-
-describe('App', () => {
-  it('Renders', () => {
-    render(<App />)
-  })
-})
